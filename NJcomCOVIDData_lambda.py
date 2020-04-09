9c1cb442cf8ad5d65f005db5ba72e70e9b42b409
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2020-04-09 01:42:57 -0400

    Added Comments and refactor for lambda

--- NJcomCOVIDData_lambda.py
+++ NJcomCOVIDData_lambda.py
@@ -1,4 +1,5 @@
 import re
+import os
 import datetime as dt
 import logging
 from urllib import request
@@ -10,12 +11,12 @@ from bs4 import BeautifulSoup as bs
 import boto3
 from botocore.exceptions import ClientError
 
-
-TODAY = dt.datetime.now() + dt.timedelta(days=0)
+# constants
+TODAY = dt.datetime.now() + dt.timedelta(days=int(os.environ['days_delta']))
 MONTHDAYYEAR = "%m-%d-%Y"
 S3NAME = 'athenedyne-covid-19'
 
-
+# RegExs for extraction
 county = re.compile(r'^(\w*[\s\w]*)\sCOUNTY.*')
 town = re.compile(r'.\s?(\w*):\s(\d*).*')
 deaths = re.compile(
@@ -28,6 +29,7 @@ recovered = re.compile(
     re.IGNORECASE
 )
 
+# Generate URL using today's date
 url = f"https://www.nj.com/coronavirus/{TODAY.strftime('%Y')}/{TODAY.strftime('%m')}/" + \
       f"where-is-the-coronavirus-in-nj-latest-map-update-on-county-by-county-cases-" + \
       f"{TODAY.strftime('%B').lower()}-{TODAY.strftime('%-d')}-" + \
@@ -58,17 +60,24 @@ def upload_file(file_name, bucket, object_name=None, ExtraArgs = {'ACL': 'public
 
 
 def lambda_handler(event, context):
+    # attempt to load generated URL
     try:
         html = bs(request.urlopen(url))
         print(f'Date: {TODAY.strftime(MONTHDAYYEAR)}\nSuccessfully opened {url}')
     except HTTPError as e:
         print(f'Date: {TODAY.strftime(MONTHDAYYEAR)}\nError trying to open {url}:\n{e}')
+        quit()
     except URLError as e:
         print(f'Date: {TODAY.strftime(MONTHDAYYEAR)}\nError trying to open {url}:\n{e}')
+        quit()
 
     current_county = ''
     towns = []
 
+    # each listing is in a <p> tag
+    # iterate over <p> to find the current county
+    # then look for town names, deaths, recoveries
+    # append to list
     for p in html.findAll('p'):
         if county.match(p.text):
             current_county = county.match(p.text).group(1).title()
@@ -92,34 +101,58 @@ def lambda_handler(event, context):
                  recovered_ct
                  ])
 
+    # convert extracted data into DF
     towns = pd.DataFrame(towns,
                          columns=['County', 'City', 'Cases',
                                   'Deaths', 'Recoveries']
                          )
     print(towns.head())
 
+    # import ZIP list from S3
     zips = pd.read_csv(f's3://{S3NAME}/NJzips.csv',
                        converters={'Zip Code': str})
     print(zips.head())
 
+    # create DF with ZIP by joining on city and county
     output = towns.merge(zips, how='left', left_on=['City', 'County'],
                          right_on=['City', 'County'])
 
     print(output.head())
 
-    output.to_csv(f'{TODAY.strftime(MONTHDAYYEAR)}-complete.csv')
+    # watch the /tmp/ folder if using local
+    # Save CSVs so they can be uploaded
+    output.to_csv(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-complete.csv')
     output[['Zip Code', 'City', 'Cases']].to_csv(
-        f'{TODAY.strftime(MONTHDAYYEAR)}-cases.csv')
-    output.groupby('Zip Code').sum().to_csv(f'{TODAY.strftime(MONTHDAYYEAR)}-zips.csv')
+        f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-cases.csv')
+    output.groupby('Zip Code').sum().to_csv(
+        f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-zips.csv')
 
-    upload_file(f'{TODAY.strftime(MONTHDAYYEAR)}-complete.csv',
-                S3NAME)
 
-    upload_file(f'{TODAY.strftime(MONTHDAYYEAR)}-cases.csv',
-                S3NAME)
+    # upload files to S3
+    upload_file(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-complete.csv',
+                S3NAME,
+                f'{TODAY.strftime(MONTHDAYYEAR)}-complete.csv')
 
-    upload_file(f'{TODAY.strftime(MONTHDAYYEAR)}-zips.csv',
-                S3NAME)
+    upload_file(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-cases.csv',
+                S3NAME,
+                f'{TODAY.strftime(MONTHDAYYEAR)}-cases.csv')
+
+    upload_file(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-zips.csv',
+                S3NAME,
+                f'{TODAY.strftime(MONTHDAYYEAR)}-zips.csv')
+
+    # duplicates as current-whatever.csv
+    upload_file(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-complete.csv',
+                S3NAME,
+                f'current-complete.csv')
+
+    upload_file(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-cases.csv',
+                S3NAME,
+                f'current-cases.csv')
+
+    upload_file(f'/tmp/{TODAY.strftime(MONTHDAYYEAR)}-zips.csv',
+                S3NAME,
+                f'current-zips.csv')
 
 
 if __name__ == '__main__':
